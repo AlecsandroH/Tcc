8dcedf4a3f27c8acdcfe120c2050cfdbb1db7cad
Etec <etec@etec.com>
2025-08-15 10:23:11 -0300

    update telaoqautismo

--- laravel/projetoTCC/resources/views/telaoqautismo.blade.php
+++ laravel/projetoTCC/resources/views/telaoqautismo.blade.php
@@ -61,8 +61,18 @@
             <button class="close-btn">&times;</button>
         </div>
         <div class="popup-content">
-            
-Texto aqui
+
+- Observação dos Sinais Precoces: Os primeiros sinais do autismo geralmente aparecem antes dos 3 anos. Os pais e cuidadores devem estar atentos a comportamentos como dificuldades na comunicação, interação social e padrões de comportamento repetitivos. 
+
+- Triagem Inicial: Muitos pediatras utilizam questionários de triagem para identificar se uma criança pode estar no espectro autista. Essa triagem pode incluir perguntas sobre o desenvolvimento da criança e seu comportamento em diferentes situações. 
+
+- Entrevista Detalhada: Um dos primeiros passos no diagnóstico é a coleta de informações sobre a história do desenvolvimento da criança. Isso pode incluir entrevistas com os pais ou cuidadores para entender melhor o comportamento da criança em casa e em ambientes sociais. 
+
+- Avaliação Multidisciplinar: Se houver suspeita de Transtorno do Espectro Autista (TEA), a criança é encaminhada para uma avaliação detalhada com especialistas, que podem incluir psicólogos, psiquiatras, fonoaudiólogos e terapeutas ocupacionais. Essa equipe multidisciplinar ajuda a garantir que todos os aspectos do desenvolvimento da criança sejam considerados. 
+
+- Uso de Escalas de Avaliação: Profissionais podem utilizar escalas de avaliação padronizadas para medir o comportamento da criança e comparar com os critérios diagnósticos do TEA. Isso pode incluir a análise de vídeos e fotos que mostram como a criança se comporta em diferentes contextos. 
+
+- Testes Complementares: Em alguns casos, testes adicionais podem ser realizados para descartar outras condições ou para entender melhor as necessidades da criança.
 
         </div>
     </div>
@@ -80,7 +90,17 @@ Texto aqui
             <button class="close-btn2">&times;</button>
         </div>
         <div class="popup-content">
-            Texto aqui
+        As principais características do autismo estão relacionadas a déficits na interação social e comunicação, incluindo:
+
+- Padrões incomuns de fala
+- Falta de contato visual
+- Não responder quando chamado pelo nome
+- Desenvolvimento tardio das habilidades de fala
+- Dificuldade em manter uma conversa
+- Repetição de frases ou palavras
+- Dificuldade em compreender os sentimentos dos outros e expressar os seus
+- Comportamentos repetitivos ou incomuns
+- Hiperfoco por determinados objetos
         </div>
     </div>
 
@@ -96,7 +116,32 @@ Texto aqui
             <button class="close-btn3">&times;</button>
         </div>
         <div class="popup-content">
-            Texto aqui
+            <p class="font">
+        O tratamento para o autismo envolve uma abordagem multidisciplinar que inclui terapias comportamentais, intervenções educacionais e, em alguns casos, medicamentos, visando melhorar a qualidade de vida e as habilidades sociais do portador.
+
+Abordagens Terapêuticas:
+
+
+<li> Terapia ABA (Análise do Comportamento Aplicada): Esta é uma das terapias mais recomendadas para crianças com Transtorno do Espectro Autista (TEA). A terapia ABA utiliza princípios de reforço positivo para desenvolver habilidades sociais e comportamentais, ajudando a reduzir comportamentos desafiadores e a melhorar a interação social. 
+</li>
+<li> Terapia Ocupacional: Foca no desenvolvimento de habilidades motoras e na promoção da independência em atividades diárias. Os terapeutas ocupacionais ajudam a melhorar a coordenação motora e a lidar com dificuldades sensoriais. 
+</li>
+<li> Fonoaudiologia: Essencial para crianças com dificuldades de comunicação, a terapia fonoaudiológica visa melhorar a fala e a interação social, utilizando técnicas adaptadas para cada criança. 
+</li>
+<li> Equoterapia: A terapia com cavalos é utilizada para melhorar a coordenação motora, a autoconfiança e a interação social, proporcionando um ambiente terapêutico único. 
+</li>
+
+Tratamentos Farmacológicos:
+
+<li> Embora não exista uma cura para o autismo, alguns medicamentos podem ser prescritos para ajudar a controlar sintomas como agressividade, ansiedade e hiperatividade.
+</li>
+Os principais medicamentos incluem:
+
+<li> Risperidona: Aprovada para reduzir a irritabilidade e comportamentos agressivos em crianças com autismo.
+</li>
+<li> Aripiprazol: Também aprovado para o controle da irritabilidade, com um perfil de efeitos colaterais mais favorávels
+</li>
+</p>
         </div>
     </div>
 </center>
