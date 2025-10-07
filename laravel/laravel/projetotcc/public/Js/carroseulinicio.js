3bbc73bdb76f11afe945002703ec9dda8e182c25
Alec <alechsbc@gmail.com>
2025-10-07 19:20:41 -0300

    tcc certo

--- laravel/laravel/projetotcc/public/Js/carroseulinicio.js
+++
@@ -1,5 +0,0 @@
-function scrollPages(direction) {
-    const container = document.getElementById("pagesWrapper");
-    const scrollAmount = 300; 
-    container.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
-}
